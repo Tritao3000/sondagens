f0b874a873372405c77a3b23b5908fa3fddd0d52
Tiago Pereira <tiagorp2004@gmail.com>
2024-02-05 15:36:03 +0000

    vite comfig

--- cnnsondagens/vite.config.js
+++ cnnsondagens/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'augusta.labs.widgets1.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'index.css') return 'augusta.labs.styles1.css';
+          return assetInfo.name;
+        },
+      },
+    },
+  },
+});
