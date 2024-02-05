96409d4f72bb648559a433ba9a0b1c0ba27df6b8
Tiago Pereira <tiagorp2004@gmail.com>
2024-02-05 12:36:41 +0000

    ready for deploy

--- cnnsondagens/src/App.jsx
+++ cnnsondagens/src/App.jsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react";
-import BarChartComponent from "./components/BarChart";
-import LineChartComponent from "./components/LineChart";
+import { useEffect, useState } from 'react';
+import BarChartComponent from './components/BarChart';
+import LineChartComponent from './components/LineChart';
 
 export const strokes = {
-  Chega: "#262626",
-  IL: "#5FB7E3",
-  Livre: "#02CD8B",
-  BE: "#C90535",
-  PS: "#F394B4",
-  AD: "#FF7F00",
-  PAN: "#008081",
-  CDU: "#1467C0",
+  Chega: '#262626',
+  IL: '#5FB7E3',
+  Livre: '#02CD8B',
+  BE: '#C90535',
+  PS: '#F394B4',
+  AD: '#FF7F00',
+  PAN: '#008081',
+  CDU: '#1467C0',
 };
 
 function App() {
-  const [sondagensHolder, setSondagensHolder] = useState(null);
-
-  useEffect(() => {
+  //const [sondagensHolder, setSondagensHolder] = useState(null);
+  const sondagensData = dataCNN;
+  /*useEffect(() => {
     async function fetchSondagens() {
       try {
-        fetch("http://localhost:8080/sondagens")
+        fetch('http://localhost:8080/sondagens')
           .then((res) => {
             return res.json();
           })
@@ -35,13 +35,13 @@ function App() {
     }
 
     fetchSondagens();
-  }, []);
+  }, []);*/
 
   return (
     <div className="max-w-4xl p-4 mx-auto">
-      <LineChartComponent data={sondagensHolder} />
+      <LineChartComponent data={sondagensData} />
       <div className="my-20" />
-      <BarChartComponent data={sondagensHolder} />
+      <BarChartComponent data={sondagensData} />
     </div>
   );
 }
