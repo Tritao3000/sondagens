978ed2219ccc0a04df586ad3cbb11306c6ed99f3
Tiago Pereira <tiagorp2004@gmail.com>
2024-02-05 15:31:15 +0000

    render graficos

--- cnnsondagens/src/main.jsx
+++ cnnsondagens/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import LineChartComponent from './components/LineChart.jsx';
+import BarChartComponent from './components/BarChart.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let sondagensBackend = sondagens;
+
+/*ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);*/
+
+ReactDOM.createRoot(document.getElementById('grafico-linhas')).render(
+  <React.StrictMode>
+    <LineChartComponent data={sondagensBackend} />
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById('grafico-barras')).render(
+  <React.StrictMode>
+    <BarChartComponent data={sondagensBackend} />
+  </React.StrictMode>
+);
